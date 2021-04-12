1a57c68da5b3e8a606fa78403f0669fa07e39943
Aleytia <jarek13705@gmail.com>
2021-04-11 22:51:41 -0700

    Update dependencies

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 docker run -d \
     --name 'Nana_Chance' \
     -v "$(pwd)/config/:/nana/config" \
+    -v "$(pwd)/data/:/nana/data" \
     --log-opt max-size=10m \
     --log-opt max-file=2 \
     nana-chance
