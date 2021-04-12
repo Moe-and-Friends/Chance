681f43169d4a219a0c35e41c584638188d3a77ea
Aleytia <jarek13705@gmail.com>
2021-04-11 22:54:30 -0700

    Update validators

--- config.py
+++ config.py
@@ -21,11 +21,10 @@ settings = Dynaconf(
     settings_files=['settings.toml', '.settings.toml', '.secrets.toml'],
     vault_enabled=False,
     validators=[
+        Validator('COMRADE_ROLE_IDS', is_type_of=list, must_exist=True),
         Validator('DISCORD_GUILD_IDS', is_type_of=list, must_exist=True),
         Validator('DISCORD_TOKEN', is_type_of=str, must_exist=True),
-        Validator('CHANCES_INVERTED_MULTIPLIER', is_type_of=int, must_exist=True),
-        Validator('CHANCES_INTERVALS', is_type_of=list, must_exist=True),
-        Validator('CHANCES_INTERVALS_ALL_START_AT_ZERO', is_type_of=bool),
+        Validator('INTERVALS', is_type_of=list, must_exist=True),
         Validator('LETHAL_ROLE_IDS', is_type_of=list),
         Validator('TRIGGERS', is_type_of=list),
         Validator('NANA_COMRADE_SAFE_MESSAGE', is_type_of=str, must_exist=True),
